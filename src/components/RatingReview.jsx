5936c8c68d69fbab6d6c00067a9e5dfeb7abeab5
B-MNSN <manutsanan0@gmail.com>
2024-09-24 22:59:53 +0700

    Add card restautant component and rating review component

---
+++ src/components/RatingReview.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+function RatingReview({ rating, setRating }) {
+    const [hoveredStar, setHoveredStar] = useState(null);
+    return (
+        <div>
+            {[1, 2, 3, 4, 5].map((star) => {
+                return (
+                    <FaStar
+                        className='start'
+                        fill={hoveredStar >= star || rating >= star ? '#f3b15a' : '#ddd'}
+                        style={{
+                            cursor: 'pointer',
+                            fontSize: '1.75rem',
+                            margin: '0 0.2rem'
+                        }}
+                        onClick={() => setRating(rating === star ? 0 : star)}
+                        onMouseEnter={() => setHoveredStar(star)}
+                        onMouseLeave={() => setHoveredStar(null)}
+                    >
+                    </FaStar>
+                )
+            })}
+        </div>
+    );
+}
+
+export default RatingReview;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,42 @@
 import Navbars from '../components/Navbar';
+import Image from 'react-bootstrap/Image';
+import defaultImage from '../assets/default_image.jpg';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+import CardRestaurant from '../components/CardRestaurant';
+
+import '../sass/home.scss';
 
 function Home() {
-    return(
+    const cards = Array.from({ length: 8 });
+
+    return (
         <>
-            <Navbars/>
+            <Navbars />
+
+            <div className='box-banner'>
+                <Image src={defaultImage} fluid />
+                <div className='box-title'>
+                    <h2>Welcome to the website,</h2>
+                    <h2>Food Order Online (FOODR) !!</h2>
+                </div>
+            </div>
+            <Container>
+                <div className='mt-5 box-restaurants'>
+                    <h2>Restaurants</h2>
+                    <Row className='mb-3'>
+                        {cards.map((_, index) => (
+                            <Col key={index} xs={12} md={6} lg={3} className="mb-4 d-flex justify-content-center">
+                                <CardRestaurant />
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            </Container>
+
         </>
     );
 }
